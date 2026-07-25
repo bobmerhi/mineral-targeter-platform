2c0de8f1aeda1ad8661f7192551c3ea1946ae0e9
bobmerhi <merhibader@gmail.com>
2026-07-25 17:31:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -174,5 +174,5 @@ with col2:
             
             complete_prompt = p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8
             
-            model = ModelInference(model_id="ibm/granite-13b-instruct-v2", credentials=credentials, project_id=PROJECT_ID)
+            model = ModelInference(model_id="meta-llama/llama-3-3-70b-instruct", credentials=credentials, project_id=PROJECT_ID)
             st.markdown(model.generate_text(prompt=complete_prompt))
